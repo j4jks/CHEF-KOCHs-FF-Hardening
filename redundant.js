456447d5506af84aaaa0f5ed5a891f90ad4c52b4
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2018-05-16 06:36:14 +0200

    redundant.js added cleanups
    Signed-off-by: CHEF-KOCH <CHEF-KOCH@protonmail.com>

---
+++ redundant.js
@@ -0,0 +1,145 @@
+/// No security benefit or outdated
+
+
+// lockPref("extensions.jid1-AVgCeF1zoVzMjA@jetpack.sdk.load.reason", "startup");
+// lockPref("general.skins.selectedSkin", "FXChrome");
+// pref("browser.tabs.remote.autostart.2", false);
+// pref("browser.uiCustomization.state", "{\"placements\":{\"PanelUI-contents\":[\"zoom-controls\",\"privatebrowsing-button\",\"save-page-button\",\"print-button\",\"fullscreen-button\",\"find-button\",\"preferences-button\",\"add-ons-button\"],\"addon-bar\":[\"addonbar-closebutton\",\"status-bar\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"nav-bar\":[\"urlbar-container\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"toolbar-menubar\":[\"menubar-items\"]},\"seen\":[\"pocket-button\",\"firefox_ghostery_com-browser-action\",\"ublock0-button\",\"developer-button\",\"webide-button\",\"action-button--jid0-9xfbwuwnvpx4wwsfbwmcm4jj69ejetpack-self-destructing-cookies\",\"disconnect-item\",\"noscript-tbb\",\"action-button--jid1-avgcef1zovzmjajetpack-random-agent-spoofer\"],\"dirtyAreaCache\":[\"PersonalToolbar\",\"nav-bar\",\"TabsToolbar\",\"toolbar-menubar\",\"PanelUI-contents\"],\"currentVersion\":6,\"newElementCount\":0}");
+// pref("e10s.rollout.cohort", "unsupportedChannel");
+// pref("extensions.disconnect.pwyw", "{\"date\":\"2016-11-02\",\"bucket\":\"viewed\"}");
+// pref("extensions.e10sBlockedByAddons", true);
+// pref("extensions.htitle.hide_mode", 2);
+// pref("extensions.https_everywhere._observatory.clean_config", true);
+// pref("extensions.https_everywhere._observatory.popup_shown", true);
+// user_pref("app.update.enabled", false);
+// user_pref("app.update.staging.enabled", false);
+// user_pref("browser.bookmarks.max_backups", 2);
+// user_pref("browser.cache.memory.capacity", 0);
+// user_pref("browser.cache.memory.enable", false);
+// user_pref("browser.chrome.favicons", false);  //does not expose you
+// user_pref("browser.chrome.site_icons", false);
+// user_pref("browser.formfill.enable", false);
+// user_pref("browser.formfill.expire_days", 0);
+// user_pref("browser.formfill.saveHttpsForms", false);
+// user_pref("browser.privatebrowsing.autostart", true);
+// user_pref("browser.safebrowsing.allowOverride", false);
+// user_pref("browser.safebrowsing.blockedURIs.enabled", false);
+// user_pref("browser.safebrowsing.downloads.enabled", false);
+// user_pref("browser.safebrowsing.downloads.remote.block_dangerous", false);
+// user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", false);
+// user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+// user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+// user_pref("browser.safebrowsing.enabled", false);
+// user_pref("browser.safebrowsing.gethashURL", "");
+// user_pref("browser.safebrowsing.malware.enabled", false);
+// user_pref("browser.safebrowsing.phishing.enabled", false);
+// user_pref("browser.safebrowsing.provider.google.gethashURL", "");
+// user_pref("browser.safebrowsing.provider.google.updateURL", "");
+// user_pref("browser.safebrowsing.provider.google4.gethashURL", "");
+// user_pref("browser.safebrowsing.provider.google4.updateURL", "");
+// user_pref("browser.safebrowsing.provider.mozilla.gethashURL", "");
+// user_pref("browser.safebrowsing.provider.mozilla.updateURL", "");
+// user_pref("browser.safebrowsing.updateURL", "");
+// user_pref("browser.search.geoip.url", "");
+// user_pref("browser.search.order.2", "");
+// user_pref("browser.search.showOneOffButtons", false);
+// user_pref("browser.sessionhistory.max_total_viewers", 0);
+// user_pref("browser.startup.page", 0); //0=blank, 2=last & 1=home
+// user_pref("browser.tabs.animate", false);
+// user_pref("browser.trackingprotection.gethashURL", "");
+// user_pref("browser.trackingprotection.updateURL", "");
+// user_pref("browser.urlbar.autocomplete.enabled", false);
+// user_pref("browser.urlbar.maxHistoricalSearchSuggestions", 0); //Omnisearch or history doesn't expose you as long the back url doesn't send pings etc.
+// user_pref("browser.urlbar.maxRichResults", 0);
+// user_pref("dom.disable_beforeunload", true);
+// user_pref("dom.disable_image_src_set", true);
+// user_pref("dom.disable_open_during_load", true);
+// user_pref("dom.enable_performance", false);
+// user_pref("dom.event.clipboardevents.enabled", false);
+// user_pref("dom.event.contextmenu.enabled", false);
+// user_pref("dom.maxHardwareConcurrency", 2);
+// user_pref("dom.mozTCPSocket.enabled", false);
+// user_pref("dom.storage.enabled", false);
+// user_pref("dom.w3c_pointer_events.enabled", false);
+// user_pref("extensions.screenshots.disabled", true);
+// user_pref("extensions.systemAddon.update.url", "");
+// user_pref("extensions.update.enabled", true);
+// user_pref("font.system.whitelist", "");
+// user_pref("full-screen-api.enabled", false);
+// user_pref("full-screen-api.warning.delay", 0);
+// user_pref("full-screen-api.warning.timeout", 0);
+// user_pref("general.autoScroll", false);
+// user_pref("javascript.options.baselinejit", false);
+// user_pref("javascript.options.ion", false);
+// user_pref("mathml.disabled", true);
+// user_pref("media.eme.enabled", false);
+// user_pref("media.gmp-eme-adobe.autoupdate", true);
+// user_pref("network.cookie.lifetime.days", 14);
+// user_pref("network.cookie.lifetimePolicy", 2);
+// user_pref("network.dns.disableIPv6", true);
+// user_pref("network.dnsCacheEntries", 400);
+// user_pref("network.dnsCacheExpiration", 60);
+// user_pref("network.http.fast-fallback-to-IPv4", true);
+// user_pref("network.http.referer.hideOnionSource", true);
+// user_pref("network.proxy.socks_remote_dns", true);
+// user_pref("offline-apps.quota.warn", 51200);
+// user_pref("permissions.memory_only", true); // (hidden pref)
+// user_pref("plugin.state.flash", 0);
+// user_pref("plugin.state.java", 0);
+// user_pref("plugins.notifyMissingFlash", false);
+// user_pref("privacy.clearOnShutdown.openWindows", true);
+// user_pref("privacy.cpd.downloads", true);
+// user_pref("privacy.cpd.openWindows", true);
+// user_pref("privacy.window.maxInnerHeight", 900);
+// user_pref("privacy.window.maxInnerWidth", 1600);
+// user_pref("reader.parse-on-load.enabled", false);
+// user_pref("security.mixed_content.block_display_content", true);
+// user_pref("security.ssl.require_safe_negotiation", true);
+// user_pref("security.ssl3.rsa_aes_128_sha", false);
+// user_pref("security.ssl3.rsa_aes_256_sha", false);
+// user_pref("services.blocklist.addons.collection", "");
+// user_pref("services.blocklist.gfx.collection", "");
+// user_pref("services.blocklist.onecrl.collection", "");
+// user_pref("services.blocklist.plugins.collection", "");
+// user_pref("shumway.disabled", true);
+// user_pref("signon.rememberSignons", false);
+// user_pref("svg.disabled", true);
+// user_pref("urlclassifier.trackingTable", "test-track-simple,base-track-digest256");
+// user_pref("urlclassifier.trackingTable", "test-track-simple,base-track-digest256,content-track-digest256");
+// user_pref("xpinstall.signatures.required", false);
+// user_pref("browser.bookmarks.showMobileBookmarks", true);
+// user_pref("browser.ctrlTab.previews", true);
+// user_pref("browser.sessionstore.privacy_level_deferred", 2);
+// user_pref("datareporting.healthreport.about.reportUrl", "");
+// user_pref("gfx.font_rendering.opentype_svg.enabled", false);
+// user_pref("intl.locale.matchOS", false); // (hidden pref)
+// user_pref("keyword.enabled", false);
+// user_pref("layers.acceleration.disabled", true);  // It's wrong that this exposes someone - myth
+// user_pref("media.video_stats.enabled", false);
+// user_pref("network.dns.blockDotOnion", true);
+// user_pref("privacy.resistFingerprinting", true); // This add-on requires a newer version of Firefox (at least version 57.0). You are using Firefox 52.0.
+// user_pref("social.whitelist", "");
+// user_pref("toolkit.cosmeticAnimations.enabled", false);
+// user_pref("xpinstall.whitelist.required", true);
+// lockPref("extensions.jid0-9XfBwUWnvPx4wWsfBWMCm4Jj69E@jetpack.sdk.load.reason", "startup");
+pref("capability.policy.maonoscript.sites", "about: about:addons about:blocked about:certerror about:config about:crashes about:feeds about:home about:memory about:neterror about:plugins about:preferences about:privatebrowsing about:sessionrestore about:srcdoc about:support blob: chrome: mediasource: moz-extension: moz-safe-about: resource:");
+
+
+// Extensions
+
+pref("extensions.agentSpoof.timeInterval", "request");
+pref("extensions.agentSpoof.uaChosen", "random_desktop");
+pref("extensions.https_everywhere.firstrun_context_menu", false);
+pref("extensions.jid0-9XfBwUWnvPx4wWsfBWMCm4Jj69E@jetpack.displayNotification", false);
+pref("extensions.jid1-AVgCeF1zoVzMjA@jetpack.show_notifications", false);
+pref("extensions.requestpolicy.initialSetupDialogShown", true);
+pref("extensions.requestpolicy.statusbarIcon", "none");
+pref("extensions.requestpolicy.uriIdentificationLevel", 3);
+pref("extensions.stylish.dbFile", "/usr/lib64/dapper-hardened-browser/defaults/profile/stylish.sqlite");
+pref("extensions.stylish.firstRun", 3);
+pref("noscript.firstRunRedirection", false);
+pref("noscript.notify", false);
+pref("noscript.notify.bottom", false);
+pref("noscript.showAddress", true);
+pref("noscript.showAllowPage", false);
+pref("noscript.showBaseDomain", false);
